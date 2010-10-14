219d39962120574967a3b88cbdeaf1af86d3e92f
Sergio Rubio <sergio@rubio.name>
2010-10-14 16:14:48 +0200

    * initial import

---
+++ build.sh
@@ -0,0 +1,12 @@
+CHROOT=centos-5.5-x86_64
+CHROOT_DIR=/var/lib/mock/$CHROOT/root
+MOCK_CMD="/usr/bin/mock --disable-plugin ccache"
+
+source build-srpm.sh
+
+$MOCK_CMD --init -r  $CHROOT
+$MOCK_CMD -r $CHROOT --rebuild rpms/SRPMS/anaconda*.rpm
+
+
+cp $CHROOT_DIR/builddir/build/RPMS/*.rpm rpms
+cp $CHROOT_DIR/builddir/build/SRPMS/*.rpm rpms/SRPMS/
